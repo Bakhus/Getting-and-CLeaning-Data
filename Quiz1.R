aa7bb2e8be49a9f7ff19eef2a4c8a4fc2a5b5b1f
Bakhtiyar <bakhtiyaruz@gmail.com>
2015-09-12 17:23:01 -0400

    Updated answers to Quiz1

--- Quiz1.R
+++ Quiz1.R
@@ -1,7 +1,7 @@
-setwd("C:\\Users\\Bahus\\Desktop\\Coursera\\Getting  and Cleaning Data")
+setwd("C:\\Users\\Bahus\\Desktop\\Coursera\\Getting  and Cleaning Data\\Getting-and-Cleaning-Data")
 
 fileUrl<-"https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv "
-
+           
 download.file(fileUrl, destfile="./survey.csv")
 
 list.files("./")
@@ -15,10 +15,14 @@ DT<-data.table(survey)
 ## Question 1
 length(DT[DT$VAL=="24",VAL])
 
+## Question 2
+
+#answer: Tidy data has one variable per column.
+
 ## Question 3
 
 fileUrl2<-"https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FDATA.gov_NGAP.xlsx"
-download.file(fileUrl2,destfile="./Gas.xlsx")
+download.file(fileUrl2,destfile="./Gas.xlsx",mode="wb")
 
 library(xlsx)
 
@@ -60,11 +64,13 @@ system.time(mean(DT$pwgtp15,by=DT$SEX))
 tapply(DT$pwgtp15,DT$SEX,mean)
 system.time(tapply(DT$pwgtp15,DT$SEX,mean))
 
+####this is the answet###########
 DT[,mean(pwgtp15),by=SEX]
 system.time(DT[,mean(pwgtp15),by=SEX])
+#################################
 
 sapply(split(DT$pwgtp15,DT$SEX),mean)
 system.time(sapply(split(DT$pwgtp15,DT$SEX),mean))
 
-system.time(rowMeans(DT)[DT$SEX==1]; rowMeans(DT)[DT$SEX==2])
+system.time({rowMeans(DT)[DT$SEX==1]; rowMeans(DT)[DT$SEX==2]})
 
